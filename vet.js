3c73909657f50fd854b87fa9473e1f3f2646aea5
Maria <Salvito1956@gmail.com>
2023-03-20 09:40:45 -0300

    modificado com o ouvi

--- vet.js
+++ vet.js
@@ -1,10 +1,21 @@
 let res = document.getElementById('res')
 
-let pais = ['Brasil, Chile, Senegal']
+let pais = ['Brasil, Chile, Senegal, Japão, China']
 let cidades = new Array("Tijucas", "Itapema")
 
-/*console.log(pais[2])*/
-/*console.log(cidades[1])*/
+console.log(pais.indexOf('Brasil'))
+
+let nome_pais = document.getElementById('nome_pais')
+let incluir = document.getElementById('incluir')
+
+incluir.addEventListener('click', ()=>{
+    res.innerHTML += pais + '<br>'
+    res.innerHTML += cidades + '<br>'
+})
+
+
+/*console.log(pais[2])
+console.log(cidades[1])
 
 pais.push('Uruguai')
 pais.unshift('Paraguai')
@@ -13,11 +24,13 @@ console.log(cidades)
 pais.pop()
 pais.shift()
 console.log(pais)
-console.log(cidades)
+console.log(cidades)*/
 
-function inserir(){
-    pais.pop()
-    pais.shift()
+/*function inserir(){
+    pais.splice(3,0,"Estados Unidos")
+    pais.splice(4,1)
+    pais.sort()
     res.innerHTML += pais + '<br>'
+    pais.reverse()
     res.innerHTML += cidades + '<br>'
-}
+}*/
